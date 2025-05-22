02b4c2d679402b0a7a8b622169402de5dbe372a6
dDiazcL <diea.diaz@duocuc.cl>
2025-05-22 21:03:02 +0000

    commit: Implementacion de Bean

--- src/main/java/com/edutech/curse_service/appConfig.java
+++ src/main/java/com/edutech/curse_service/appConfig.java
@@ -1,11 +1,13 @@
 package com.edutech.curse_service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class appConfig {
 
+    @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
