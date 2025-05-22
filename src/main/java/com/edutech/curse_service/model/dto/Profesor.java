72f42ff747091c349bd47c88cd5c041d10d9b486
dDiazcL <diea.diaz@duocuc.cl>
2025-05-22 20:58:48 +0000

    Commit: Implementaciones Finales

---
+++ src/main/java/com/edutech/curse_service/model/dto/Profesor.java
@@ -0,0 +1,12 @@
+package com.edutech.curse_service.model.dto;
+
+import lombok.Data;
+
+@Data
+public class Profesor {
+
+    private Long id;
+    private String nombreUsuario;
+    private String email;
+
+}
--- src/main/java/com/edutech/curse_service/service/CursoService.java
+++ src/main/java/com/edutech/curse_service/service/CursoService.java
@@ -3,11 +3,11 @@ package com.edutech.curse_service.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.edutech.curse_service.model.Curso;
+import com.edutech.curse_service.model.dto.Profesor;
 import com.edutech.curse_service.repository.CursoRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -15,30 +15,34 @@ import jakarta.persistence.EntityNotFoundException;
 @Service
 
 public class CursoService {
-    @Autowired
-    private CursoRepository cursoRepository;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final CursoRepository cursoRepository;
+    private final RestTemplate restTemplate;
 
-    private static final String USER_SERVICE_URL = "http:locahost:8082/profesores";
+    private static final String USER_SERVICE_URL = "http://localhost:8082/profesores/";
 
-    public List<Curso>findCursosByIdProfesor(Long idProfesor) {
+    public CursoService(CursoRepository cursoRepository, RestTemplate restTemplate) {
+        this.cursoRepository = cursoRepository;
+        this.restTemplate = restTemplate;
+    }
+
+    public List<Curso> findCursosByIdProfesor(Long idProfesor) {
         try {
+            // Validar existencia del profesor
             String url = USER_SERVICE_URL + idProfesor;
-            restTemplate.getForObject(url,Object.class);
+            Profesor profesor = restTemplate.getForObject(url, Profesor.class);
+            if (profesor == null || profesor.getId() == null) {
+                throw new RuntimeException("Profesor no encontrado");
+            }
         } catch (Exception e) {
-            throw new RuntimeException("Profesor con ID " + idProfesor + "No encontrado en el microservicio de usuario");
+            throw new RuntimeException("Error al consultar el profesor con ID " + idProfesor);
         }
+
             List<Curso> cursos = cursoRepository.findByIdProfesor(idProfesor);
         if (cursos.isEmpty()) {
         throw new RuntimeException("No se encontraron cursos para el profesor con ID" + idProfesor);
         }
         return cursos;
-    }
-
-    public CursoService(CursoRepository cursoRepository) {
-        this.cursoRepository = cursoRepository;
     }
 
     public List<Curso> getAllCursos() {
